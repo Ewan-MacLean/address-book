56dd015bff685ae307a4cd82695fdf2dea215983
Ewan MacLean <git config --global user.email ewanmaclean77@gmail.comgit config --global user.email>
2022-03-25 11:26:33 -0300

    .

--- src/components/ContactList/contactList.js
+++ src/components/ContactList/contactList.js
@@ -3,8 +3,6 @@ import Contact from "../Contact/contact"
 const ContactList = function(props) {
     return(
         <div className="contactlist-container">
-
-            {/* need to format the syntax here with "(" because combining JSX and JS is tricky */}
             {props.contacts.map(contact=>(
                     <Contact key={contact.firstname} image={contact.image} firstName={contact.firstname} lastName={contact.lastname} 
                     email={contact.email} phone={contact.phone}  />
