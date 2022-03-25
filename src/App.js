eb0eed85096229ce0de6f7bd760a10da973eac35
Ewan MacLean <git config --global user.email ewanmaclean77@gmail.comgit config --global user.email>
2022-03-25 10:18:25 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Contact from './components/Contact/contact';
+import ContactList from './components/ContactList/contactList';
 
 function App() {
+
+  const [data,setData] = useState([])
+
+  useEffect(()=>{
+    fetch('http://localhost:3000/contacts').then(x=>x.json().then(x=>setData(x)))
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ContactList contacts={data}/>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/ContactList/contactList.js
@@ -0,0 +1,19 @@
+import Contact from "../Contact/contact"
+
+const ContactList = function(props) {
+    return(
+        <div className="contactlist-container">
+
+            {/* need to format the syntax here with "(" because combining JSX and JS is tricky */}
+            {props.contacts.map(contact=>(
+                    <Contact key={contact.firstname} image={contact.price} firstName={contact.firstname} lastName={contact.lastname} 
+                    email={contact.email} phone={contact.phone}  />
+                    )
+                )
+            }
+
+        </div>
+    )
+}
+
+export default ContactList
